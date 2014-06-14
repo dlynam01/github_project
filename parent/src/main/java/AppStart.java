9f8bb1d096170d1e08b510a33a3b4ac8a59038df
dlynam01 <davidlynam1@hotmail.co.uk>
2014-06-14 20:08:09 +0100

    this is a new commit totally thru intellij

--- parent/src/main/java/AppStart.java
+++ parent/src/main/java/AppStart.java
@@ -6,5 +6,6 @@ public class AppStart {
     public static void main(String[] args){
         System.out.println("Hello world");
         System.out.println("This is a change");
+        System.out.println("This is totally thru intellij");
     }
 }
